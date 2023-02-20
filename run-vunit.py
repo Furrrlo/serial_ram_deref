33474c17b890a5858a4e0a02b9f5d6481b747ac6
Furrrlo <ferloo@protonmail.com>
2023-02-20 15:39:30 +0100

    Add teacher's provided testbench to vunit

--- run-vunit.py
+++ run-vunit.py
@@ -6,4 +6,8 @@ lib = vu.add_library("lib")
 lib.add_source_files("vunit-tests/*.vhd")
 lib.add_source_files("serial_to_parallel_ram_dereference.srcs/sources_*/**/*.vhd")
 
+# Support for std_logic_unsigned
+vu.add_compile_option("ghdl.a_flags", ["-fsynopsys"])
+vu.set_sim_option("ghdl.elab_flags", ["-fsynopsys"])
+
 vu.main()
